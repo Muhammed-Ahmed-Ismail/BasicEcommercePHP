73d8f42733896c8e2af712f06ce9579257551479
modafro <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2022-03-09 19:53:53 +0200

    create get user by id and email service

--- Services/UserService.php
+++ Services/UserService.php
@@ -14,13 +14,31 @@ class UserService
         $users = array();
 
         $result = $this->dbContext->getDbc()::table("users")->get();
-        foreach ($result as $user => $data) {
+        foreach ($result as $data) {
             $tempUser = new UserDto();
             $tempUser->setId($data->ID);
             $tempUser->setEmail($data->e_mail);
             $users[] = $tempUser;
         }
         return $users;
+    }
+
+    public function getUserById($id): UserDto
+    {
+        $selectedUser = new UserDto();
+        $result = $this->dbContext->getDbc()::table('users')->where("ID", $id)->first();
+        $selectedUser->setId($result->ID);
+        $selectedUser->setEmail($result->e_mail);
+        return $selectedUser;
+    }
+
+    public function getUserByEmail($email)
+    {
+        $selectedUser = new UserDto();
+        $result = $this->dbContext->getDbc()::table('users')->where("e_mail", $email)->first();
+        $selectedUser->setId($result->ID);
+        $selectedUser->setEmail($result->e_mail);
+        return $selectedUser;
     }
 
 }
