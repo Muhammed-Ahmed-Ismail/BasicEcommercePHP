d667edfb89c213f8a88821e683feacfd68230a11
modafro <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2022-03-18 00:28:55 +0200

    remove redundunt download link

--- Utilities/Routes.php
+++ Utilities/Routes.php
@@ -21,15 +21,12 @@ class Routes
             case '/create-product':
                 require_once("./Views/CreateProduct.php");
                 break;
-            case '/download-s3':
+            case '/download':
                 require_once("./Views/download.php");
                 break;
-            case '/download':
-                require_once ("./Views/downloadpage.php");
-                break;
             case  '/logout':
-                 require_once ("./Views/logout.php");
-                 break;
+                require_once("./Views/logout.php");
+                break;
             default:
                 http_response_code(404);
                 require_once("./Views/404.php");
