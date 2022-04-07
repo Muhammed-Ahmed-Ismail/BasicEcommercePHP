157bf12b4e1b74bdfcb116ba3245e204a4c6fcf6
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-04-07 21:52:46 +0200

    final

--- Views/downloadpage.php
+++ Views/downloadpage.php
@@ -1,17 +1,9 @@
 <?php
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
+
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) {
     $orderservice = new OrderServices();
     $productService = new ProductServices();
     $orderId = $_SESSION["order_id"];
-    $count = $orderservice->getDownloadTimes($orderId)->downloads_count;
-
-
-      //  $orderservice->updateAnyProduct(1, $orderId, $count + 1);
-
-        print_r($_GET);
-      //  header('Content-Disposition: attachment; filename ="' . "product" . '.zip"');
         if (isset($_GET["local"])) {
             $productService->downloadFromLocalServer($orderId,1);
 
@@ -19,10 +11,4 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) {
             $s3Link = $productService->getObjectDownloadLink();
             header("Location:$s3Link");
         }
-/*
-        header('Content-Disposition: attachment; filename ="' . "product" . '.zip"');
-        readfile("Download_resources/7HC7kZrlX3.zip");*/
-
-
-
 }
--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
 session_start();
 require_once("vendor/autoload.php");
-//include("ProjectUtilities/Routes.php");
-
-
-/*$userService = new UserService();
-$userService->insertUser("a","1");*/
-/*$orserservice=new OrderServices();
-$orserservice->addOrder(1,1);*/
-
 Routes::router();
